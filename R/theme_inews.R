b824c54cf2c7c9cd846f276f8ea2b31987a6224b
Tom Saunders <tomjs@protonmail.com>
2021-12-15 14:13:44 +0000

    * Reformatted basic theme * added option for direct labels * detect device from filename * Added fonts for axis/labels/annotations * forced update of default settings on package load

--- R/theme_inews.R
+++ R/theme_inews.R
@@ -6,6 +6,24 @@ library(stats)
 library(ggplot2)
 library(ggrepel)
 library(paletteer)
+library(ggtext)
+library(directlabels)
+library(stringr)
+
+.onLoad <- function(libname, pkgname) {
+  # to show a startup message
+  ggplot2::update_geom_defaults("text", list(family = "St Ryde", colour="#898c89", size = 0.8))
+  ggplot2::update_geom_defaults("label", list(family = "St Ryde", colour="#898c89", size = 0.8))
+  ggplot2::update_geom_defaults("line", list(colour="#E33A11"))
+  #font_import(prompt = FALSE, pattern = "Bitter|Stryde")
+  extrafont::loadfonts(device = "win", quiet = TRUE)
+}
+
+inews_lab <- function(label, size=0.6, gap = 0.2){
+  directlabels::geom_dl(aes(label = {{label}}), method = list(dl.trans(x = x + 0.2), cex=size, fontfamily = "Stag", "last.points"))
+}
+
+
 
 #' Rolling Average
 #' @param n The number of days to execute rolling average over
@@ -30,16 +48,20 @@ scale_fill_inews <- function(...) {
   discrete_scale("fill", "inews", inews_pal(), ...)
 }
 
+
+
+
 #' Inews basic theme
 #'
 #' @param base_size Basic size of graph, defaults to 25
 #' @param base_family base font family, not implemented
-theme_inews_basic <- function(base_size = 25, base_family="", fill="White") {
-  if(fill == "White"){
-    f_val = "#ffffff"
-  }
-  if(fill == "Grey") {
+theme_inews_basic <- function(base_size = 25, base_family="", fill="white") {
+  if(fill == "none") {
+    f_val = NA
+  }else if (fill == "grey"){
     f_val = "#f0f0f0"
+  }else{
+    f_val = "#ffffff"
   }
   theme_minimal(base_size = base_size, base_family = base_family) %+replace%
     theme(
@@ -59,68 +81,36 @@ theme_inews_basic <- function(base_size = 25, base_family="", fill="White") {
       legend.margin = margin(t = 0,r = 0, b = 0,l =0, unit = "pt"),
       legend.box.margin = margin(t = 5,r = 0, b = 0,l =0, unit = "pt"),
       legend.box.spacing = unit(4, "points"),
-      legend.text = element_text(size = rel(0.9)),
+      legend.text = element_text(size = rel(0.6)),
 
 
       #Formatting axis
       axis.title = element_blank(),
-      axis.text = element_text(size = rel(0.7), margin=margin(0,0,0,0, unit="pt")),
+      axis.text = element_text(size = rel(0.7), margin=margin(0,0,0,0, unit="pt"), family = "Stag"),
       axis.ticks = element_line(size = rel(0.7), colour = "#878787"),
       axis.ticks.y = element_blank(),
       axis.ticks.length = unit(5, "points"),
-      axis.line.x = element_line(colour = "#878787", size = rel(0.5)),
+      axis.line = element_line(colour = "#878787", size = rel(0.5)),
+      axis.line.y = ggplot2::element_blank(),
 
       #Format backgrounds + panels
-      panel.background = element_rect(linetype = 0),
+      panel.background = element_rect(linetype = 0, fill=f_val),
       panel.border = element_blank(),
       panel.grid.major = element_line(colour = "#e0e1e2", size = rel(0.5)),
       panel.grid.major.x = element_blank(),
       panel.grid.minor = element_blank(),
 
       #formatting text
-      plot.title = element_text(size = rel(1.2), colour = "#000000", family = "Bitter", face="bold", hjust = 0),
-      text = element_text(size=rel(1), family="Bitter", colour = "#898a8c"),
-      plot.subtitle = element_text(size = rel(1.1), colour = "#525354", family="Bitter", hjust=0, margin=margin(t=5)),
-      plot.caption = element_text(family="Bitter", colour = "#898a8c", size = rel(0.5), hjust = 0)
-
+      plot.title = element_text(size = rel(2), colour = "#000000", family = "Bitter Black", hjust = 0.5),
+      text = element_text(size=rel(1), family="Stag", colour = "#898a8c"),
+      plot.subtitle = element_text(size = rel(0.8), colour = "#9C9C9C", family="Bitter Regular", hjust=0.5, margin=margin(t=8, b=5, unit="pt")),
+      plot.caption = ggtext::element_markdown(family="Stag", colour = "#898a8c", size = rel(0.5), hjust = 0, margin=margin(t=5, unit="pt")),
+      complete = TRUE
     )
 
 }
 
-#' Inews ggparlaiment theme
-#'
-#' @param base_size Basic size of graph, defaults to 25
-#' @param base_family base font family, not implemented
-theme_inews_parl <- function(base_size = 25, base_family="") {
-  theme_void(base_size = base_size, base_family = base_family) %+replace%
-    theme(
-      #Format Legend
-      legend.title = element_blank(),
-      legend.position = "top",
-      legend.direction = "horizontal",
-      legend.justification = "left",
-      legend.spacing = unit(0, "points"),
-      legend.key.size = unit(1, "lines"),
-      legend.key.height = NULL,
-      legend.key.width = NULL,
-      legend.margin = margin(t = 0, r = 5, b = 0, l = 5, unit = "pt"),
-      legend.box.margin = margin(t = 5, r = 0, b = 0, l = 0, unit = "pt"),
-      legend.box.spacing = unit(4, "points"),
-      legend.text = element_text(size = 10),
-      #Format plot (with margins)
-      plot.title.position = "panel",
-      plot.margin = margin(t = 0, r = 5, b = 0, l = 5, unit = "cm"),
-      plot.background = element_rect(fill = f_val, colour=NA),
 
-      #Format titles/text formatting
-      plot.title = element_text(size = rel(15), colour = "#000000", family = "Bitter", face = "bold", hjust = 0),
-      text = element_text(size = rel(1), family = "Bitter", colour = "#898a8c"),
-      plot.subtitle = element_text(size = rel(10), colour = "#525354", family = "Bitter", hjust = 0, margin=margin(t=5)),
-      plot.caption = element_text(family = "Bitter", colour = "#898a8c", size = rel(0.5), hjust = 0)
-      )
-
-
-}
 
 theme_inews_facet <- function(base_size = 25, base_family="") {
   theme_inews_basic(base_size = base_size, base_family = base_family) %+replace%
@@ -162,7 +152,7 @@ theme_inews_map <- function(base_size = 25, base_family="", fill="White"){
       plot.title = element_text(size = rel(2), colour = "#000000", family = "Bitter", face = "bold",  hjust = 0),
       text = element_text(size = rel(1), family = "Bitter", colour = "#898a8c"),
       plot.subtitle = element_text(size = rel(1.3), colour = "#525354", family = "Bitter", hjust = 0, margin=margin(t=5)),
-      plot.caption = element_text(family = "Bitter", colour = "#898a8c", size = rel(1), hjust = 0),
+      plot.caption = element_markdown(family = "Bitter", colour = "#898a8c", size = rel(1), hjust = 0),
 
       #Add small margin
       plot.margin = margin(t = 10,r = 10, b = 10,l =10, unit = "pt")
@@ -176,62 +166,11 @@ theme_inews_map <- function(base_size = 25, base_family="", fill="White"){
 #' @param height_i Specified height in cm, defaults to 10
 #' @param type Adds presets for maps/parls and other types to render
 #' @param l_size Enables to turn off limiting size
-save_inews <- function(filename, plot=last_plot(), width_i = 15, height_i = 10, type="basic", l_size=TRUE, expand=FALSE, device="png", units_i="cm", adj = TRUE){
+save_inews <- function(filename, plot=last_plot(), width = 15, height = 11, type="basic", units="cm", labs="none"){
   cap_all <- ggplot2::ggplot_build(plot)
 
-  ################ Auto Formatting Legends ###############
-  if (adj == TRUE){
-    plotAndPrintRatio <- function(g, width, height) {
-      gGrob <- ggplot2::ggplotGrob(g)
-      tmpfile <- tempfile(pattern = "png")
-      png(tmpfile, width = width, height = height, units=units_i, res=300) # it is necessary to open a device
-      plot(g)
-      legendSize <- as.numeric(grid::convertWidth(grid::grobWidth(gGrob$grobs[[15]]), unitTo = "inches"))
-      plotSize <-   as.numeric(grid::convertWidth(grid::grobWidth(gGrob$grobs[[7]]), unitTo = "inches"))
-      # the ratio of legend size to plot size
-      dev.off()
-      return(legendSize / plotSize)
-    }
-    if (type == "basic"){
-      val <- plotAndPrintRatio(plot, width_i, height_i)
-      if (is.numeric(val) & is.na(val) == F){
-        if(val > 1){
-          rows = ceiling(val)
-          pos_tps <- names(cap_all[[3]][[9]])
-          if ("colour" %in% pos_tps){
-            plot <- plot + guides(colour = guide_legend(nrow = rows))
-          }
-          if ("fill" %in% pos_tps){
-            plot <- plot + guides(fill = guide_legend(nrow = rows))
-          }
-          if ("size" %in% pos_tps){
-            plot <- plot + guides(size = guide_legend(nrow = rows))
-          }
-          if ("linetype" %in% pos_tps){
-            plot <- plot + guides(linetype = guide_legend(nrow = rows))
-          }
-          if ("alpha" %in% pos_tps){
-            plot <- plot + guides(alpha = guide_legend(nrow = rows))
-          }
-        }
-      }
-    }
-  }
 
-
-
-
-  # Add copyright
-
-  cap <-  cap_all[[3]][[9]]$caption
-  cap <- paste(cap, "By Tom Saunders", sep="\n")
-  newcap <- paste(cap, "© i", sep="\n")
-  plot <- plot +
-    labs(caption = newcap)
-
-
-
-  # Add presets:
+  ## Presets for different graph types
   if (type == "map"){
     height_i = 25
     width_i = 25
@@ -241,33 +180,61 @@ save_inews <- function(filename, plot=last_plot(), width_i = 15, height_i = 10, 
     width_i = 20
   }
 
-  # Ensure scales are not expanded
-  if (expand == TRUE & plot$coordinates$default == T ){
 
-      plot <- plot +
-        coord_cartesian(expand = FALSE)
+
+  ## Adding label space
+  if(labs != "none" & is.numeric(labs)){
+    plot <- plot +
+      theme(
+        legend.position = "none",
+        plot.margin = margin(r = labs, unit = "cm")
+        )
+    width = width + (labs - 1)
   }
+
+
+
+
+  # Add copyright
+
+  cap <-  cap_all[[3]][[9]]$caption
+  newcap <- paste(cap, "<br>By Tom Saunders <span style='font-family:Arial'> · ©</span><span style='font-family:Bitter; color:#E33A11;'><b> i</b></span> ", sep="")
+  plot <- plot +
+    labs(caption = newcap)
+
+
+
+  # Remove clipping if default coords
+  if (plot$coordinates$default == T) {
+    plot <- plot +
+      coord_cartesian(clip = 'off')
+  }
+
+  # Set device from filename
+  device = stringr::str_extract(filename, "(?<=\\.).+")
+
+
+
+
+
+
   if(device == "png"){
-    ggsave(filename, plot, dpi = 300, type = "cairo", width = width_i, height = height_i, units = units_i, limitsize = l_size)
+    ggsave(filename, plot, dpi = 300, type = "cairo", width = width, height = height, units = units, limitsize = FALSE)
   }
   if(device == "eps"){
-    ggsave(filename, plot, dpi = 300, device=cairo_ps, width = width_i, height = height_i, units = units_i, limitsize = l_size)
+    ggsave(filename, plot, dpi = 300, device=cairo_ps, width = width, height = height, units = units, limitsize = FALSE)
 
   }
   if(device == "svg"){
-    ggsave(filename, plot, dpi = 300, device=svg, width = width_i, height = height_i, units = units_i, limitsize = l_size)
+    ggsave(filename, plot, dpi = 300, device=svg, width = width, height = height, units = units, limitsize = FALSE)
   }
 }
 
 
 #' Update ggplot defaults for text etc. to Inews
-set_default_inews <- function(){
-  ggplot2::update_geom_defaults("text", list(family = "Bitter", colour="#898c89", size = 2))
-  ggplot2::update_geom_defaults("label", list(family = "Bitter", colour="#898c89", size = 2))
-  ggplot2::update_geom_defaults("text_repel", list(family = "Bitter", colour="#898c89", size = 2))
-  ggplot2::update_geom_defaults("label_repel", list(family = "Bitter", colour="#898c89", size = 2))
-  ggplot2::update_geom_defaults("line", list(colour="#E33A11", size=1.5))
-}
+
+
+
 
 
 #' Binned scale (scale_fill_fermenter wrapper with better gradients)
